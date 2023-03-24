9ac30854eb5c7240c150876ccad77ecc1a417b98
Jonathan Plasse <13716151+JonathanPlasse@users.noreply.github.com>
2023-03-25 00:43:30 +0100

    Expend lint API

--- src/lib.rs
+++ src/lib.rs
@@ -3,24 +3,58 @@ use std::path::Path;
 use pyo3::exceptions::PyException;
 use pyo3::prelude::*;
 use ruff::linter::{lint_only, LinterResult};
+use ruff::settings::configuration::Configuration;
 use ruff::settings::flags::{Autofix, Noqa};
+use ruff::settings::options::Options;
 use ruff::settings::Settings;
-use wrappers::Message;
 
-mod wrappers;
+use crate::message::Message;
+
+mod message;
+
+fn into_pyerr(e: impl std::fmt::Display) -> PyErr {
+    PyException::new_err(e.to_string())
+}
 
 #[pyfunction]
-fn lint(content: &str) -> PyResult<Vec<Message>> {
+fn lint(
+    content: &str,
+    settings: &str,
+    path: Option<&str>,
+    noqa: Option<bool>,
+    autofix: Option<bool>,
+    package: Option<&str>,
+) -> PyResult<Vec<Message>> {
+    let path = path.map(Path::new).unwrap_or(Path::new("-"));
+    let options: Options = toml::from_str(settings).map_err(into_pyerr)?;
+    let configuration = Configuration::from_options(options, path).map_err(into_pyerr)?;
+    let settings = Settings::from_configuration(configuration, path).map_err(into_pyerr)?;
+    let noqa = noqa.map_or(Noqa::Enabled, |v| {
+        if v {
+            Noqa::Enabled
+        } else {
+            Noqa::Disabled
+        }
+    });
+    let autofix = autofix.map_or(Autofix::Disabled, |v| {
+        if v {
+            Autofix::Enabled
+        } else {
+            Autofix::Disabled
+        }
+    });
+    let package = package.map(Path::new);
+
     let LinterResult { data, error } = lint_only(
         content,
-        Path::new("-"),
-        None,
-        &Settings::default(),
-        Noqa::Enabled,
-        Autofix::Disabled,
+        path,
+        package,
+        &settings,
+        noqa,
+        autofix,
     );
     if let Some(error) = error {
-        Err(PyException::new_err(error.to_string()))
+        Err(into_pyerr(error))
     } else {
         Ok(data.into_iter().map(Message::from).collect())
     }
